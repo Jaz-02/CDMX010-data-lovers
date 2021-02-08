abdd9dd3f679ac89f1453af69ec5d9a39300812f
jazmin <tegan01sara12@gmail.com>
2021-02-08 12:54:16 -0600

    cambios de pruebas

--- src/data.js
+++ src/data.js
@@ -1,9 +1,95 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+export const rickandmorty = () => {
+  return 'rickandmorty';
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+export function filterAlive(statusAlive, status) {
+
+  const filterForAlive= statusAlive.filter(allData=> allData.status===status);
+  
+  return filterForAlive;
 };
+
+export function filterDead(statusDead, status) {
+
+  const filterForDead= statusDead.filter(allData=> allData.status===status);
+  
+  return filterForDead;
+};
+
+export function filterFemale(genderFemale, gender) {
+
+  const filterForFemale= genderFemale.filter(b => b.gender===gender);
+  
+  return filterForFemale;
+};
+
+export function filterMale(genderMale, gender) {
+
+  const filterForMale= genderMale.filter(allData=> allData.gender===gender);
+  
+  return filterForMale;
+};
+ 
+
+ 
+// export function orderAz(nameOrder, name) { 
+
+//   const oorderSort = nameOrder.sort(allData => allData.name===name);
+//  const orderSort= nameOrder.sort( allData => allData.name===name);
+//  return (oorderSort < orderSort); 
+
+//     const orderToAZ = nameOrder.sort((a-b) => allData.name ====name); 
+
+//  return (a.name < b.name);
+ 
+     
+//  };
+ 
+   /*Const oorderSort= nameOrder.sort( a => a.name===name);
+ const orderSort= nameOrder.sort( b => b.name===name);
+ return (oorderSort < orderSort); */
+/* const orderToAZ = nameOrder.sort((a-b) => allData.name ====name); */
+
+// function myFunction() {
+//   fruits.sort();
+//   fruits.reverse();
+// }
+
+
+// var items = ['réservé', 'premier', 'cliché', 'communiqué', 'café', 'adieu'];
+// items.sort(function (a, b) {
+//   return a.localeCompare(b);
+// });
+
+// 
+
+export function orderAz(s){
+  const orderSort= s.sort((a, b) =>{
+    if (a.name > b.name ) {
+      return 1;
+    }
+    if (a.name < b.name){
+      return -1;
+    }
+    return 0;
+  });
+  return orderSort;
+};
+// // console.log(orderAz);
+
+// export function orderZa(sZa){
+//   const orderSortZa= sZa.sort((c, d) =>{
+//     if (c.name < d.name ) {
+//       return 1;
+//     }
+//     if (c.name > d.name){
+//       return -1;
+//     }
+//     return 0;
+//   });
+//    return orderSortZa;
+// };
+
+//console.log(orderZa);
--- src/main.js
+++ src/main.js
@@ -1,7 +1,208 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-//import data from './data/pokemon/pokemon.js';
- import data from './data/rickandmorty/rickandmorty.js';
- 
+import data from './data/rickandmorty/rickandmorty.js';
+import {rickandmorty, filterAlive, filterDead, filterFemale, filterMale, orderAz, } from './data.js';
 
-console.log(example, data);
+console.log(rickandmorty);
+
+const $dataCards= document.querySelector(".information"),
+      $template= document.getElementById("card").content,
+      $fragment= document.createDocumentFragment(),
+      char= data.results;
+
+      char.forEach( (allData) => {
+          $template.querySelector("h2").textContent = allData.name;
+          $template.querySelector("img").setAttribute("src",allData.image);
+          $template.querySelector(".a1").textContent = allData.status;
+          $template.querySelector(".a2").textContent = allData.species;
+          $template.querySelector(".a3").textContent = allData.type;
+          $template.querySelector(".a4").textContent = allData.gender;
+          $template.querySelector(".a5").textContent = allData.origin.name;
+          $template.querySelector(".a6").textContent = allData.location.name;
+
+          let $clone = document.importNode($template, true);
+            $fragment.appendChild($clone);
+      });
+      $dataCards.appendChild($fragment);
+     
+//FILTRADO VIVOS//
+
+      const statusAlive = data.results;
+      console.log(filterAlive(statusAlive, 'Alive'));
+      let bo = document.getElementById("alive");
+      bo.addEventListener("click", () =>  {
+         document.getElementById("b").style.display = "none";
+         document.getElementById("c").style.display = "block";
+        let dataFilter= filterAlive(statusAlive, 'Alive');
+        const $template= document.getElementById("aliveFilter").content,
+              $dataCards= document.querySelector(".infoFilter"),
+              $fragment= document.createDocumentFragment();
+      
+       //Toda la data//       
+              dataFilter.forEach( (allData) => {
+                $template.querySelector("h2").textContent = allData.name;
+                $template.querySelector("img").setAttribute("src",allData.image);
+                $template.querySelector(".a1").textContent = allData.status;
+                $template.querySelector(".a2").textContent = allData.species;
+                $template.querySelector(".a3").textContent = allData.type;
+                $template.querySelector(".a4").textContent = allData.gender;
+                $template.querySelector(".a5").textContent = allData.origin.name;
+                $template.querySelector(".a6").textContent = allData.location.name;
+      
+                let $clone = document.importNode($template, true);
+                  $fragment.appendChild($clone);
+                
+            });
+      
+            $dataCards.appendChild($fragment);
+      });
+//FILTRADO MUERTOS
+      const statusDead = data.results;
+     console.log(filterDead(statusDead, 'Dead'));
+      let boo = document.getElementById("dead");
+      boo.addEventListener("click", () =>  {
+         document.getElementById("b").style.display = "none";
+         document.getElementById("c").style.display = "block";
+        let dataFilter = filterDead(statusDead, 'Dead');
+        const $template= document.getElementById("aliveFilter").content,
+              $dataCards= document.querySelector(".infoFilter"),
+              $fragment= document.createDocumentFragment();
+      
+       //Toda la data//       
+              dataFilter.forEach( (allData) => {
+                $template.querySelector("h2").textContent = allData.name;
+                $template.querySelector("img").setAttribute("src",allData.image);
+                $template.querySelector(".a1").textContent = allData.status;
+                $template.querySelector(".a2").textContent = allData.species;
+                $template.querySelector(".a3").textContent = allData.type;
+                $template.querySelector(".a4").textContent = allData.gender;
+                $template.querySelector(".a5").textContent = allData.origin.name;
+                $template.querySelector(".a6").textContent = allData.location.name;
+      
+                let $clone = document.importNode($template, true);
+                  $fragment.appendChild($clone);
+                
+            });
+      
+            $dataCards.appendChild($fragment);
+      });
+
+      const genderFemale = data.results;
+     console.log(filterFemale(genderFemale, 'Female'));
+      let booo = document.getElementById("female");
+      booo.addEventListener("click", () =>  {
+         document.getElementById("b").style.display = "none";
+         document.getElementById("c").style.display = "block";
+        let dataFilter = filterFemale(genderFemale, 'Female');
+        const $template= document.getElementById("aliveFilter").content,
+              $dataCards= document.querySelector(".infoFilter"),
+              $fragment= document.createDocumentFragment();
+      
+       //Toda la data//       
+              dataFilter.forEach( (allData) => {
+                $template.querySelector("h2").textContent = allData.name;
+                $template.querySelector("img").setAttribute("src",allData.image);
+                $template.querySelector(".a1").textContent = allData.status;
+                $template.querySelector(".a2").textContent = allData.species;
+                $template.querySelector(".a3").textContent = allData.type;
+                $template.querySelector(".a4").textContent = allData.gender;
+                $template.querySelector(".a5").textContent = allData.origin.name;
+                $template.querySelector(".a6").textContent = allData.location.name;
+      
+                let $clone = document.importNode($template, true);
+                  $fragment.appendChild($clone);
+                
+            });
+      
+            $dataCards.appendChild($fragment);
+      });
+
+
+      const genderMale = data.results;
+     console.log(filterMale(genderMale, 'Male'));
+      let boooo = document.getElementById("male");
+      boooo.addEventListener("click", () =>  {
+         document.getElementById("b").style.display = "none";
+         document.getElementById("c").style.display = "block";
+        let dataFilter = filterMale(genderMale, 'Male');
+        const $template= document.getElementById("aliveFilter").content,
+              $dataCards= document.querySelector(".infoFilter"),
+              $fragment= document.createDocumentFragment();
+                  
+              dataFilter.forEach( (allData) => {
+                $template.querySelector("h2").textContent = allData.name;
+                $template.querySelector("img").setAttribute("src",allData.image);
+                $template.querySelector(".a1").textContent = allData.status;
+                $template.querySelector(".a2").textContent = allData.species;
+                $template.querySelector(".a3").textContent = allData.type;
+                $template.querySelector(".a4").textContent = allData.gender;
+                $template.querySelector(".a5").textContent = allData.origin.name;
+                $template.querySelector(".a6").textContent = allData.location.name;
+      
+                let $clone = document.importNode($template, true);
+                  $fragment.appendChild($clone);
+                
+            });
+      
+            $dataCards.appendChild($fragment);
+      });
+
+
+//PRUEBAS//
+
+const nameOrder = data.results; 
+     console.log(orderAz(nameOrder,'name'));
+     let abcOrder = document.getElementById("abc");
+   abcOrder.addEventListener("click", () => {
+      document.getElementById("b").style.display = "none";
+      document.getElementById("c").style.display = "block";
+  let orderData = orderAz(nameOrder, 'name');
+  const $template= document.getElementById("aliveFilter").content,
+  $dataCards= document.querySelector(".infoFilter"),
+  $fragment= document.createDocumentFragment()
+
+  orderData.forEach( (allData) => {
+    $template.querySelector("h2").textContent = allData.name;
+    $template.querySelector("img").setAttribute("src",allData.image);
+    $template.querySelector(".a1").textContent = allData.status;
+    $template.querySelector(".a2").textContent = allData.species;
+    $template.querySelector(".a3").textContent = allData.type;
+    $template.querySelector(".a4").textContent = allData.gender;
+    $template.querySelector(".a5").textContent = allData.origin.name;
+    $template.querySelector(".a6").textContent = allData.location.name;
+
+    let $clone = document.importNode($template, true);
+      $fragment.appendChild($clone);
+    
+});
+
+$dataCards.appendChild($fragment);
+});
+
+//  de la Z a la A//
+// const nameOrderZa = data.results; 
+//      console.log(orderZa(nameOrderZa,'name'));
+  
+//    let abcOrderZa = document.getElementById("abc");
+//    abcOrderZa.addEventListener("click", () => {
+//       //aquí va lo que oculta la data//
+//   let orderDataZa = orderZa(nameOrderZa, 'name');
+//   const $template= document.getElementById("aliveFilter").content,
+//   $dataCards= document.querySelector(".infoFilter"),
+//   $fragment= document.createDocumentFragment()
+
+//   orderDataZa.forEach( (allData) => {
+//     $template.querySelector("h2").textContent = allData.name;
+//     $template.querySelector("img").setAttribute("src",allData.image);
+//     $template.querySelector(".a1").textContent = allData.status;
+//     $template.querySelector(".a2").textContent = allData.species;
+//     $template.querySelector(".a3").textContent = allData.type;
+//     $template.querySelector(".a4").textContent = allData.gender;
+//     $template.querySelector(".a5").textContent = allData.origin.name;
+//     $template.querySelector(".a6").textContent = allData.location.name;
+
+//     let $clone = document.importNode($template, true);
+//       $fragment.appendChild($clone);
+    
+// });
+
+// $dataCards.appendChild($fragment);
+// });
